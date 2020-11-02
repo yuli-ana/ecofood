70782acb5a47190cfd83b12c0b3634cfb5431926
Yuliana Hazda <yulianahazda@gmail.com>
2020-11-02 18:01:33 -0500

    Add router and routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
-function App() {
-  return <h1>YOUFOOD</h1>;
-}
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./pages/home";
+import Restaurant from "./pages/restaurant";
+import User from "./pages/user";
+
+const App = () => {
+  return (
+    <>
+      <Router>
+        <Switch>
+          <Route path="/user" exact component={User} />
+          <Route path="/restaurant" exact component={Restaurant} />
+          <Route path="/" exact component={Home} />
+        </Switch>
+      </Router>
+    </>
+  );
+};
 
 export default App;
