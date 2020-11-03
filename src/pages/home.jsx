a544f03cc9ec4b9a9e5e05454c49c3de3805c59b
Yuliana Hazda <yulianahazda@gmail.com>
2020-11-03 15:42:56 -0500

    Use useHistory hook to navigate views on click, add event handlers

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,26 @@
 import { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 import NavBar from "../components/shared/Navbar";
 
 const Home = () => {
   const [restaurants, setRestaurants] = useState([]);
-  return <NavBar />;
+  const history = useHistory();
+
+  const handleUserClick = () => {
+    history.push("/user");
+  };
+
+  const handleRestaurantClick = () => {
+    history.push("/restaurant");
+  };
+
+  return (
+    <>
+      <NavBar />
+      <button onClick={() => handleUserClick()}>User</button>
+      <button onClick={() => handleRestaurantClick()}>Restaurant</button>
+    </>
+  );
 };
 
 export default Home;
