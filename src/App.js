c167f0d83095a4db05ad6f810a2ceb0970ee33b3
Yuliana Hazda <yulianahazda@gmail.com>
2020-11-04 12:22:39 -0500

    Add new routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/home";
 import SignIn from "./components/home/SignIn";
-import SingUpRestaurant from "./components/home/signup/SingUpRestaurant";
-import SingUpUser from "./components/home/signup/SingUpUser";
+import SignUp from "./components/home/signup/SignUp";
+import SingupUser from "./components/home/signup/SingupUser";
+import DishModal from "./components/menu/DishModal";
+import SingupRestaurant from "./components/home/signup/SingupRestaurant";
+import Menu from "./pages/menu";
+import SearchRestaurants from "./pages/searchRestaurants";
 
 const App = () => {
   return (
     <>
       <Router>
         <Switch>
-          <Route path="/sign-in" component={SignIn} />
-          <Route path="/signup-restaurant" component={SingUpRestaurant} />
-          <Route path="/signup-user" component={SingUpUser} />
+          <Route path="/modal" component={DishModal} />
+          <Route path="/signin" component={SignIn} />
+          <Route path="/signup/user" component={SingupUser} />
+          <Route path="/signup/restaurant" component={SingupRestaurant} />
+          <Route path="/signup" component={SignUp} />
+          <Route path="/menu" component={Menu} />
+          <Route path="/search-restaurants" component={SearchRestaurants} />
           <Route path="/" component={Home} />
         </Switch>
       </Router>
