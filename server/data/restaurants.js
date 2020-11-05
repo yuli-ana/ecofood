3de320e4e4fcab27533d954a25e8c5403ad2f81d
Yuliana Hazda <yulianahazda@gmail.com>
2020-11-05 14:04:28 -0500

    Add restaurants database

---
+++ server/data/restaurants.js
@@ -0,0 +1,28 @@
+const restaurants = [
+  {
+    id: 283,
+    name: "Cora's Breakfast and Lunch",
+    currency: "$",
+    dishes: [
+      { name: "Brown butter mayo, sourdough", price: 18 },
+      { name: "Cremini mushroom, chimichurri", price: 22 },
+      { name: "Applewood smoked cheddar", price: 16 },
+      { name: "Jalapeño hoe cakes, Yuzu honey, hot sauce", price: 18 },
+      { name: "daily selection of cured meats and cheeses", price: 24 },
+    ],
+  },
+  {
+    id: 382,
+    name: " LaVinia Restaurant",
+    currency: "$",
+    dishes: [
+      { name: "Grilled Calamari with Chilli oil", price: 16.75 },
+      { name: "Sautéed Shrimp with Garlic and Olive Oil", price: 17.75 },
+      { name: "Fine Ibérico Spanish Ham", price: 25.75 },
+      { name: "Sautéed Mushrooms in Garlic and Olive oil", price: 13.75 },
+      { name: "Fresh Mussells Steamed in Spanish Beer", price: 15.75 },
+    ],
+  },
+];
+
+module.exports = restaurants;
