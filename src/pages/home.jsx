409547afbdd56e7867bac17957d671e03ed0c7c0
Yuliana Hazda <yulianahazda@gmail.com>
2020-11-05 14:07:14 -0500

    Test axios get request, data is succesfully returned from server

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,8 +1,22 @@
-import { useState } from "react";
-import { useHistory } from "react-router-dom";
+import { useState, useEffect } from "react";
 import NavBar from "../components/shared/Navbar";
+import restService from "../services/restaurants";
 
 const Home = () => {
+  const [restaurants, setRestaurants] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const data = await restService.getAll();
+      console.log("data got returned");
+      setRestaurants(data);
+    };
+
+    getData();
+  }, []);
+
+  console.log(restaurants);
+
   return (
     <>
       <NavBar />
