4858087800ddb10cdbb95d54bc7276851ad8c2e7
Yuliana Hazda <yulianahazda@gmail.com>
2020-11-04 23:14:56 -0500

    Updates

---
+++ restaurantsData.js
@@ -0,0 +1,151 @@
+const restaurants = [
+  {
+    id: 123,
+    name: "",
+    currency: "$",
+    dishes: [
+      { name: "Brown butter mayo, sourdough", price: 18 },
+      { name: "Cremini mushroom, chimichurri", price: 22 },
+      { name: "Applewood smoked cheddar", price: 16 },
+      { name: "Jalapeño hoe cakes, Yuzu honey, hot sauce", price: 18 },
+      { name: "daily selection of cured meats and cheeses", price: 24 },
+    ],
+  },
+  {
+    id: 321,
+    name: "Aloette",
+    currency: "$",
+    dishes: [
+      { name: "Brown butter mayo, sourdough", price: 18 },
+      { name: "Cremini mushroom, chimichurri", price: 22 },
+      { name: "Applewood smoked cheddar", price: 16 },
+      { name: "Jalapeño hoe cakes, Yuzu honey, hot sauce", price: 18 },
+      { name: "daily selection of cured meats and cheeses", price: 24 },
+    ],
+  },
+  {
+    id: 456,
+    name: "Madrina Bar Y Tapas",
+    currency: "$",
+    dishes: [
+      {
+        name:
+          "Catalonian crispy bread rubbed with tomato, extra virgin olive oil",
+        price: 7,
+      },
+      {
+        name:
+          "Mediterranean herbs, espelette pepper, piquillo pepper ratatouille, valdeon cheese sauce",
+        price: 19,
+      },
+      { name: "Curry scented chickpea hummus, smoked paprika", price: 18 },
+      {
+        name:
+          "Sandwich of crispy baby squid, black ink aioli, pickled piparra pepper",
+        price: 9,
+      },
+      { name: "Shrimp carpaccio, lemon zest", price: 17 },
+    ],
+  },
+  {
+    id: 654,
+    name: "Sunset Grill",
+    currency: "$",
+    dishes: [
+      {
+        name:
+          "GREEK OMELETTE: Feta cheese, onion, Kalamata olives, and red pepper",
+        price: 16,
+      },
+      {
+        name:
+          "FLORENTINE OMELETTE: Fresh baby spinach, onion, and Swiss cheese",
+        price: 18,
+      },
+      {
+        name: "SUNSET OMELETTE: Swiss cheese, peameal-style bacon and tomato",
+        price: 16,
+      },
+      {
+        name:
+          "MEDITERRANEAN OMELETTE: Fresh baby spinach, tomato and Feta cheese",
+        price: 15,
+      },
+      { name: "WESTERN: Ham and onion", price: 12 },
+    ],
+  },
+  {
+    id: 789,
+    name: "IQ cafe",
+    currency: "$",
+    dishes: [
+      { name: "Brown butter mayo, sourdough", price: 18 },
+      { name: "Cremini mushroom, chimichurri", price: 22 },
+      { name: "Applewood smoked cheddar", price: 16 },
+      { name: "Jalapeño hoe cakes, Yuzu honey, hot sauce", price: 18 },
+      { name: "daily selection of cured meats and cheeses", price: 24 },
+    ],
+  },
+  {
+    id: 987,
+    name: "Sushi on Bloor",
+    currency: "$",
+    dishes: [
+      { name: "Brown butter mayo, sourdough", price: 18 },
+      { name: "Cremini mushroom, chimichurri", price: 22 },
+      { name: "Applewood smoked cheddar", price: 16 },
+      { name: "Jalapeño hoe cakes, Yuzu honey, hot sauce", price: 18 },
+      { name: "daily selection of cured meats and cheeses", price: 24 },
+    ],
+  },
+  {
+    id: 901,
+    name: "The Keg Steakhouse",
+    currency: "$",
+    dishes: [
+      { name: "Brown butter mayo, sourdough", price: 18 },
+      { name: "Cremini mushroom, chimichurri", price: 22 },
+      { name: "Applewood smoked cheddar", price: 16 },
+      { name: "Jalapeño hoe cakes, Yuzu honey, hot sauce", price: 18 },
+      { name: "daily selection of cured meats and cheeses", price: 24 },
+    ],
+  },
+  {
+    id: 109,
+    name: "Queen Marherita pizza",
+    currency: "$",
+    dishes: [
+      { name: "Brown butter mayo, sourdough", price: 18 },
+      { name: "Cremini mushroom, chimichurri", price: 22 },
+      { name: "Applewood smoked cheddar", price: 16 },
+      { name: "Jalapeño hoe cakes, Yuzu honey, hot sauce", price: 18 },
+      { name: "daily selection of cured meats and cheeses", price: 24 },
+    ],
+  },
+  {
+    id: 283,
+    name: "Cora's Breakfast and Lunch",
+    currency: "$",
+    dishes: [
+      { name: "Brown butter mayo, sourdough", price: 18 },
+      { name: "Cremini mushroom, chimichurri", price: 22 },
+      { name: "Applewood smoked cheddar", price: 16 },
+      { name: "Jalapeño hoe cakes, Yuzu honey, hot sauce", price: 18 },
+      { name: "daily selection of cured meats and cheeses", price: 24 },
+    ],
+  },
+  {
+    id: 382,
+    name: " LaVinia Restaurant",
+    currency: "$",
+    dishes: [
+      { name: "Grilled Calamari with Chilli oil", price: 16.75 },
+      { name: "Sautéed Shrimp with Garlic and Olive Oil", price: 17.75 },
+      { name: "Fine Ibérico Spanish Ham", price: 25.75 },
+      { name: "Sautéed Mushrooms in Garlic and Olive oil", price: 13.75 },
+      { name: "Fresh Mussells Steamed in Spanish Beer", price: 15.75 },
+    ],
+  },
+];
+
+module.exports = restaurants;
