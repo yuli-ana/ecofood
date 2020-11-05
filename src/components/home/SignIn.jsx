9f2195d675cfa01a2eefac2011c28d237c69eecc
Yuliana Hazda <yulianahazda@gmail.com>
2020-11-04 23:24:00 -0500

    Updates

--- src/components/home/SignIn.jsx
+++ src/components/home/SignIn.jsx
@@ -1,27 +1,10 @@
 import NavBar from "../shared/Navbar";
-import { Link as RouterLink } from "react-router-dom";
-import { Button } from "@material-ui/core";
 
 const SignIn = () => {
   return (
     <>
       <NavBar />
-      <Button
-        style={{ marginTop: "100px" }}
-        color="primary"
-        component={RouterLink}
-        to="/signup/user"
-      >
-        User
-      </Button>
-      <Button
-        style={{ marginTop: "100px" }}
-        color="primary"
-        component={RouterLink}
-        to="/signup/restaurant"
-      >
-        Restaurant
-      </Button>
+      <div style={{ paddingTop: "100px" }}>Sign In</div>
     </>
   );
 };
