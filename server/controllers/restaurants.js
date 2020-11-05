34203438a79b7bae155b1061553d42cc59275abb
Yuliana Hazda <yulianahazda@gmail.com>
2020-11-05 14:09:31 -0500

    Updates

--- server/controllers/restaurants.js
+++
@@ -1,7 +0,0 @@
-const data = require("../restaurantsData");
-const express = require("express");
-const router = express.Router();
-
-router.get("/api/restaurants", (req, res) => {
-  res.send("Hello world");
-});
