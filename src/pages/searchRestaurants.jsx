40239a622f29d4799f125900ff5b586d80a9e123
Yuliana Hazda <yulianahazda@gmail.com>
2020-11-05 14:06:01 -0500

    Remove unused imports

--- src/pages/searchRestaurants.jsx
+++ src/pages/searchRestaurants.jsx
@@ -1,10 +1,6 @@
-import { useState, useEffect } from "react";
 import NavBar from "../components/shared/Navbar";
-import restaurantService from "../services/restaurants";
 
 const SearchRestaurants = () => {
-  const [restaurants, setRestaurants] = useState([]);
-
   return (
     <>
       <NavBar minimalNavbar />
