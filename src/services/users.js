18af720bbc8fcd10383134298c87b1de41e1fa2d
Yuliana Hazda <yulianahazda@gmail.com>
2020-11-05 14:04:10 -0500

    Update axios response

--- src/services/users.js
+++ src/services/users.js
@@ -4,22 +4,22 @@ const baseUrl = "/api/users";
 
 const getAll = async () => {
   const response = await axios.get(baseUrl);
-  return await response.data;
+  return await response.json();
 };
 
 const create = async (newObject) => {
   const response = await axios.post(baseUrl, newObject);
-  return await response.data;
+  return await response.json();
 };
 
 const update = async (id, newObject) => {
   const response = await axios.put(`${baseUrl}/${id}`);
-  return await response.data;
+  return await response.json();
 };
 
 const deleteUser = async (id) => {
   const response = await axios.delete(`${baseUrl}/${id}`);
-  return await response.data;
+  return await response.json();
 };
 
 export default { getAll, create, update, deleteUser };
