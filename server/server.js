c798dd3f11d3beceaa847ec23a60a0db59678fb9
Yuliana Hazda <yulianahazda@gmail.com>
2020-11-05 14:03:28 -0500

    Import all necessary libraries and routers

--- server/server.js
+++ server/server.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const app = express();
+const restaurantRouter = require("./routes/restaurantRoutes");
 const cors = require("cors");
+const userRouter = require("./routes/usersRoutes");
 const mongoose = require("mongoose");
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/restaurants", restaurantRouter);
+app.use("/api/users", userRouter);
 
 module.exports = app;
