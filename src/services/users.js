6e7986e894bf4664b888aa1cd6b38e0883b4bfe1
Yuliana Hazda <yulianahazda@gmail.com>
2020-11-05 20:14:38 -0500

    Updates

--- src/services/users.js
+++ src/services/users.js
@@ -1,25 +1,26 @@
 import axios from "axios";
 
-const baseUrl = "/api/users";
+const baseUrl = "http://localhost:3001/api/users";
 
 const getAll = async () => {
   const response = await axios.get(baseUrl);
-  return await response.json();
+  return await response.data;
 };
 
 const create = async (newObject) => {
   const response = await axios.post(baseUrl, newObject);
-  return await response.json();
+  return await response.data;
 };
 
 const update = async (id, newObject) => {
-  const response = await axios.put(`${baseUrl}/${id}`);
-  return await response.json();
+  const response = await axios.put(`${baseUrl}/${id}`, newObject);
+  return await response.data;
 };
 
 const deleteUser = async (id) => {
   const response = await axios.delete(`${baseUrl}/${id}`);
-  return await response.json();
+  return await response.data;
 };
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default { getAll, create, update, deleteUser };
