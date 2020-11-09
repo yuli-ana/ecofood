c3b482d9f08853388d51b739b14f6ff8ff82b6ab
Yuliana Hazda <yulianahazda@gmail.com>
2020-11-09 13:06:16 -0500

    Shorten URL

--- src/services/restaurants.js
+++ src/services/restaurants.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:3001/api/restaurants";
+const baseUrl = "/api/restaurants";
 
 const getAll = async () => {
   const response = await axios.get(baseUrl);
