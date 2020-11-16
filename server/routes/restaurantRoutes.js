208f822939cdb46d89e477e5f30a5448c194f1f6
Yuliana Hazda <yulianahazda@gmail.com>
2020-11-16 17:58:45 -0500

    Import mongoose models

--- server/routes/restaurantRoutes.js
+++ server/routes/restaurantRoutes.js
@@ -2,6 +2,9 @@ const express = require("express");
 const restaurantRouter = express.Router();
 const restaurantsData = require("../data/restaurants");
 
+// Restaurant mongoose model
+const Restaurant = require("../models/Restaurants");
+
 restaurantRouter.get("/", (req, res) => {
   res.json(restaurantsData);
 });
