ad88a1565e2386f1f0b4134c68dae5f71217d257
Yuliana Hazda <yulianahazda@gmail.com>
2020-11-16 17:59:30 -0500

    Access to env variables

--- server/utils/config.js
+++ server/utils/config.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 
+// Access environment variables
 const PORT = process.env.API_PORT;
+const MONGODB_URI = process.env.MONGODB_URI;
 
-module.exports = { PORT };
+module.exports = { PORT, MONGODB_URI };
