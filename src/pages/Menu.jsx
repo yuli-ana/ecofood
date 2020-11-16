cd54732767b93a3cf2f32f768ffc681ffa9d7416
Yuliana Hazda <yulianahazda@gmail.com>
2020-11-16 13:50:47 -0500

    Get id with useParams, find restaurant by id, pass data down to DishList

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,10 +1,30 @@
+import { useState, useEffect } from "react";
 import NavBar from "../components/shared/Navbar";
+import DishList from "../components/menu/DishList";
+import restService from "../services/restaurants";
+import { useParams } from "react-router-dom";
 
 const Menu = () => {
+  const { id } = useParams();
+  const [restData, setData] = useState([]);
+
+  // Find restaurant that matches url id
+  const restaurantData = restData.find((rest) => rest.id === id);
+
+  useEffect(() => {
+    const getData = async () => {
+      const data = await restService.getAll();
+      setData(data);
+    };
+
+    getData();
+  }, []);
+
   return (
     <>
       <NavBar signed />
-      <p style={{ marginTop: "200px" }}>Menu</p>
+      <h2 style={{ marginTop: "200px" }}>Menu</h2>
+      <DishList data={restaurantData} />
     </>
   );
 };
