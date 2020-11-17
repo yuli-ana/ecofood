d011de7dd54d78fda34d6388414cd9e29f040490
Yuliana Hazda <yulianahazda@gmail.com>
2020-11-17 14:34:31 -0500

    Update Schemas, add validation

--- server/models/Users.js
+++ server/models/Users.js
@@ -3,11 +3,14 @@ const { Schema } = mongoose;
 
 // User Schema
 const userSchema = new Schema({
-  name: String,
-  age: Number,
-  weight: Number,
-  height: Number,
+  name: { type: String, required: true },
+  age: { type: Number, required: true },,
+  gender: { type: String, required: true },
+  phone: { type: Number, min: 10, max: 15 },
+  weight: { type: Number, required: true },
+  height: { type: Number, required: true },
   dishes: [String],
+  updated: { type: Date, default: Date.now },
 });
 
 const User = mongoose.model("User", userSchema);
