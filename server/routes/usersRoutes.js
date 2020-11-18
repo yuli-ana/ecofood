792251f4aa0b5e2d55357d4e13ef16c517dd5bf0
Yuliana Hazda <yulianahazda@gmail.com>
2020-11-18 17:21:48 -0500

    Update route handler

--- server/routes/usersRoutes.js
+++ server/routes/usersRoutes.js
@@ -17,19 +17,36 @@ usersRouter.get("/", async (req, res) => {
 //   res.json(user);
 // });
 
-usersRouter.post("/", async (req, res) => {
+usersRouter.post("/add", async (req, res) => {
   console.log(req.body);
+  console.log("successful request");
 
   const newUser = new User({
     ...req.body,
+    dishes: [],
   });
 
-  try {
-    const user = await newUser.save();
-    res.json(user);
-  } catch (err) {
-    res.status(400).send("Bad request");
-  }
+  // try {
+  //   const user = await newUser.save();
+  //   res.json(user);
+  // } catch (err) {
+  //   res.status(400).send("Bad request");
+  // }
+
+  const user = await newUser.save();
+  res.json(user);
 });
 
+usersRouter.post("/:id", async (req, res) => {
+  const { userId } = req.params;
+  const { size, name, price, id } = req.body;
+
+  console.log("done");
+
+  const user = await User.findById(userId);
+
+  console.log(user);
+
+  user.dishes.push();
+});
 module.exports = usersRouter;
