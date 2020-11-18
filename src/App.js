4ff25338a21a05a07390554d4ba68db09280fad0
Yuliana Hazda <yulianahazda@gmail.com>
2020-11-18 17:17:31 -0500

    Add a global state, utilize useContext

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useState, createContext } from "react";
 import Home from "./pages/home";
 import SignIn from "./components/home/SignIn";
 import SignUp from "./components/home/signup/SignUp";
@@ -13,22 +14,31 @@ const theme = createMuiTheme({
   spacing: [0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 16, 32, 64],
 });
 
+export const Context = createContext();
+
 const App = () => {
+  const [userId, setUserId] = useState("");
+
+  // Log user id
+  console.log(userId, "Global userId");
+
   return (
-    <ThemeProvider theme={theme}>
-      <Router>
-        <Switch>
-          <Route path="/modal" component={DishModal} />
-          <Route path="/signin" component={SignIn} />
-          <Route path="/signup/user" component={SignupUser} />
-          <Route path="/signup/restaurant" component={SignupRestaurant} />
-          <Route path="/signup" component={SignUp} />
-          <Route path="/restaurants/:id" component={Menu} />
-          <Route path="/restaurants" component={SearchRestaurants} />
-          <Route path="/" component={Home} />
-        </Switch>
-      </Router>
-    </ThemeProvider>
+    <Context.Provider value={[userId, setUserId]}>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <Switch>
+            <Route path="/modal" component={DishModal} />
+            <Route path="/signin" component={SignIn} />
+            <Route path="/signup/user" component={SignupUser} />
+            <Route path="/signup/restaurant" component={SignupRestaurant} />
+            <Route path="/signup" component={SignUp} />
+            <Route path="/restaurants/:id" component={Menu} />
+            <Route path="/restaurants" component={SearchRestaurants} />
+            <Route path="/" component={Home} />
+          </Switch>
+        </Router>
+      </ThemeProvider>
+    </Context.Provider>
   );
 };
 
