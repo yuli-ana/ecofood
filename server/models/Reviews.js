835a2fd51d8805cf707cf3cb99b2e210754fdcee
Yuliana Hazda <yulianahazda@gmail.com>
2020-11-19 16:04:19 -0500

    Add a new  schema for user reviews

---
+++ server/models/Reviews.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
+const { Schema } = mongoose;
+
+const reviewSchema = new Schema({
+  portionSize: String,
+  dishId: String,
+  restaurantId: String,
+});
+
+const Review = mongoose.model("Review", reviewSchema);
+
+reviewSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+    // the passwordHash should not be revealed
+    delete returnedObject.passwordHash;
+  },
+});
+
+reviewSchema.plugin(uniqueValidator);
+
+module.exports = Review;
