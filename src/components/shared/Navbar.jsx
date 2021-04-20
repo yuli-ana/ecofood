0fcac3c3b7466993680232f7fcf6fe8cdc148336
Yuliana Hazda <yulianahazda@gmail.com>
2021-04-20 13:47:54 -0400

    add onClick handler, refactor

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,6 +1,8 @@
 import { Link as RouterLink } from "react-router-dom";
 import { AppBar, Toolbar, Button } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import { useContext } from "react";
+import { AuthContext } from "../../services/context/authContext";
 
 const useStyles = makeStyles({
   root: {
@@ -15,33 +17,30 @@ const useStyles = makeStyles({
   },
 });
 
-const NavBar = ({ signed }) => {
+const NavBar = () => {
   const classes = useStyles();
 
   return (
     <AppBar position="fixed" className={classes.root}>
       <Toolbar className={classes.toolbar}>
         <NavLink path="/" text="home" />
-        {!signed ? (
-          <NavLink path="/accounts/login" text="log in" />
-        ) : (
-          <>
-            <NavLink path="/restaurants" text="search" />
-            <NavLink path="/" text="log out" />
-          </>
-        )}
-        {!signed && <NavLink path="/accounts/signup" text="sign up" />}
+        <NavLink path="/restaurants" text="search" />
+        <NavLink path="/accounts/login" text="log in" />
+        <NavLink path="/" text="log out" />
       </Toolbar>
     </AppBar>
   );
 };
 
 const NavLink = ({ path, text }) => {
+  const { setAuthData } = useContext(AuthContext);
+
   const classes = useStyles();
   return (
     <Button
       className={classes.button}
       variant="outlined"
+      onClick={() => (text === "log out" ? setAuthData(null) : null)}
       component={RouterLink}
       to={path}
     >
