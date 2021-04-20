06698a7589b95d419db5cc9abd0c69c66b64db8c
Yuliana Hazda <yulianahazda@gmail.com>
2021-04-20 13:48:11 -0400

    update styles

--- src/components/login/LoginPage.jsx
+++ src/components/login/LoginPage.jsx
@@ -1,9 +1,11 @@
-import NavBar from "../shared/Navbar";
 import Login from "./Login";
 import { Grid } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles({
+  root: {
+    position: "relative",
+  },
   image: {
     height: "100vh",
     width: "100%",
@@ -14,24 +16,38 @@ const useStyles = makeStyles({
     background: "deeppink",
     margin: "0 auto",
   },
+  exitButton: {
+    position: "absolute",
+    top: "20px",
+    right: "20px",
+  },
+  btnColor: {
+    color: "#000",
+  },
+  btnBack: {
+    textAlign: "center",
+    fontSize: "12px",
+  },
 });
 
 const LoginPage = () => {
   const classes = useStyles();
 
   return (
-    <Grid container>
-      <Grid item xs={6}>
-        <img
-          className={classes.image}
-          src="https://images.unsplash.com/photo-1478145046317-39f10e56b5e9?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=934&q=80"
-          alt=""
-        />
+    <div className={classes.root}>
+      <Grid container>
+        <Grid item xs={6}>
+          <img
+            className={classes.image}
+            src="https://images.unsplash.com/photo-1478145046317-39f10e56b5e9?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=934&q=80"
+            alt=""
+          />
+        </Grid>
+        <Grid container item xs={6} justify="center" alignContent="center">
+          <Login />
+        </Grid>
       </Grid>
-      <Grid container item xs={6} justify="center" alignContent="center">
-        <Login />
-      </Grid>
-    </Grid>
+    </div>
   );
 };
 
